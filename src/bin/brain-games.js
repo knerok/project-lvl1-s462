a2e011c94030ca879397ba5d04f276d5c22f98fd
Andre Berto <knerok@gmail.com>
2019-03-21 21:40:22 +0300

    finally

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { resultt } from '..';
+import asd from '..';
 
-resultt();
+asd();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 
 import readlineSync from 'readline-sync';
 
-export const resultt = () => {
+const resultt = () => {
   console.log('Welcome to the Brain Games! \n');
   const actual = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${actual}!`);
+  return 'adasd';
 };
+export default resultt;
