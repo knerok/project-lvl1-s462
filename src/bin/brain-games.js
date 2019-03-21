715727e5d1bb27ba5d0e7ad509e5f76a141b8639
Andre Berto <knerok@gmail.com>
2019-03-21 17:27:22 +0300

    third task

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
-import {result} from '..'
+import { result } from '..';
+
 result();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-
+#!/usr/bin/env node
 import readlineSync from 'readline-sync';
+
 export const result = () => {
-console.log('Welcome to the Brain Games! \n');
-const actual = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${actual}!`);
-return ;
-}
+  console.log('Welcome to the Brain Games! \n');
+  const actual = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${actual}!`);
+};
