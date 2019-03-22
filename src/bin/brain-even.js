ebf474401928306f51db3fec5d6d81bd53466438
Andre Berto <knerok@gmail.com>
2019-03-22 12:03:58 +0300

    corrections4

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,26 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import asd from '../games/even';
 
-console.log('Welcome to the Brain Games!');
-console.log('Answer "yes" if number even otherwise answer "no".\n');
-const actual = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${actual}!\n`);
-let indicator = 1;
-let random = 0;
-let answer = '';
-let corAnswer = '';
-while (indicator > 0 && indicator <= 3) {
-  random = Math.floor(Math.random() * 500000) + (-100000);
-  console.log(`Question: ${random}`);
-  answer = readlineSync.question('Your answer: ');
-  random = random < 0 ? random * (-1) : random;
-  corAnswer = random % 2 === 0 ? 'yes' : 'no';
-  if ((answer === corAnswer && random % 2 === 0) || (answer === corAnswer && random % 2 === 1)) {
-    console.log('Correct!');
-    indicator += 1;
-  } else {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${corAnswer}'. \nLet's try again, ${actual}!`);
-    indicator = 0;
-  }
-  if (indicator === 4) console.log(`Congratulations, ${actual}!`);
-}
+asd();
---
+++ src/games/even.js
@@ -0,0 +1,34 @@
+import readlineSync from 'readline-sync';
+
+const generateRandom = (min, max) => Math.floor(Math.random() * (max + 1 - min)) + min;
+
+const isEven = (n) => {
+  const p = n < 0 ? n * (-1) : n;
+  return p % 2 === 0;
+};
+
+const solution = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no".\n');
+  const actual = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${actual}!\n`);
+  let indicator = 0;
+  let random = 0;
+  let answer = '';
+  let correctAnswer = '';
+  while (indicator >= 0 && indicator < 3) {
+    random = generateRandom(-100000, 100000);
+    console.log(`Question: ${random}`);
+    answer = readlineSync.question('Your answer: ');
+    correctAnswer = isEven(random) ? 'yes' : 'no';
+    if (answer === correctAnswer) {
+      console.log('Correct!');
+      indicator += 1;
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. \nLet's try again, ${actual}!`);
+      indicator = -1;
+    }
+    if (indicator === 3) console.log(`Congratulations, ${actual}!`);
+  }
+};
+export default solution;
