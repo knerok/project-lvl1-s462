47c76421a70bfbffe5bed600e58bed940a8f846b
Andre Berto <knerok@gmail.com>
2019-03-24 00:47:18 +0300

    please

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -14,7 +14,7 @@ const generateDataGameGcd = () => {
   const random1 = generateRandom(0, 100000);
   const random2 = generateRandom(0, 100000);
   const question = `${random1} ${random2}`;
-  const correctAnswer = getGcd(Math.abs(random1), Math.abs(random2));
+  const correctAnswer = getGcd(random1, random2);
   return [question, correctAnswer];
 };
 
